9ea6780f3e9f57de70eb2ca2c7d919db24e93924
mcopik <mcopik@gmail.com>
2016-12-20 09:32:44 +0100

    Blaze: remove old code from example

--- lin_alg/blaze_force_eval.cpp
+++ lin_alg/blaze_force_eval.cpp
@@ -2,39 +2,20 @@
 #include <chrono>
 #include <blaze/Blaze.h>
 
-// inf norm of matrix
-//template<typename Matrix>
-double norm(const blaze::DynamicMatrix<double, blaze::rowMajor> & m)
-{
-	//typedef typename Matrix::ElementType FloatType;
-	typedef double FloatType;	
-	FloatType res = 0.0;
-	//No generic iterator + padding in matrix -> no STL algorithms
-	for(size_t i = 0; i < m.rows(); ++i) {
-		FloatType sum = std::accumulate(m.begin(i), m.end(i), 0.0,
-			[](FloatType sum, FloatType elem) {
-				return sum + std::abs(elem);
-			});
-		res = std::max(res, sum);
-	}
-
-	return res;
-}
-
 int main()
 {
-    typedef std::chrono::high_resolution_clock clock_t;
+	typedef std::chrono::high_resolution_clock clock_t;
 	typedef blaze::DynamicMatrix<double, blaze::rowMajor> mat_t;
 	const int rows = 400, cols = 300;
 
 	mat_t A(rows, cols), B(rows, cols);
 
 	// init	
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(0, 1);
-    A = blaze::forEach(A, [&](double) { return dis(gen); });
-    B = blaze::forEach(B, [&](double) { return dis(gen); });
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> dis(0, 1);
+	A = blaze::forEach(A, [&](double) { return dis(gen); });
+	B = blaze::forEach(B, [&](double) { return dis(gen); });
 	
 	// Don't assume return type - no computation
 	auto clock_begin = clock_t::now();
